077bd5468ad403980df637f3fafe0e1bea825670
Yuri Victorovich <yuri@FreeBSD.org>
2024-12-09 22:22:18 -0800

    Fix type mismatches in freebsd.c

--- c/freebsd.c
+++ c/freebsd.c
@@ -28,7 +28,7 @@ const char *get_os_release(void) {
 	return (os_release);
 }
 
-uint64_t get_cpu_speed(void) {
+unsigned long get_cpu_speed(void) {
 	uint64_t tsc_freq;
 	size_t len;
 	int error;
@@ -44,7 +44,7 @@ uint64_t get_cpu_speed(void) {
 	return (tsc_freq / 1000 / 1000);
 }
 
-uint64_t get_proc_total(void) {
+unsigned long get_proc_total(void) {
 	struct kinfo_proc *kp, *kpp;
 	int mib[3], count, error;
 	size_t len;
